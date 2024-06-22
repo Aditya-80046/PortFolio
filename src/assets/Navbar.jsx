5128121e21d036268838359b632ea40802e64708
Aditya Srivastava <srv.aditya2985@gmail.com>
2024-06-22 12:31:10 +0530

    Navbar created

---
+++ src/assets/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <>
+      <div className="container nav-bar">
+        <div className="left nav-items">Portfolio</div>
+        <div className="right">
+          <a href="" className="nav-items">Home</a>
+          <a href="" className="nav-items">Skills</a>
+          <a href="" className="nav-items">Projects</a>
+          <a href="" className="nav-items">Contact</a>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
